e0088a733751042f1678cea165bb0c81fbdc1b8b
YosefTamer <programmeryosef@gmail.com>
2026-02-06 15:09:01 +0200

    Refactor FundMe contract and update imports

--- fund_me_smart_contract/FundMe.sol
+++ fund_me_smart_contract/FundMe.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.18;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
-import {PriceConverter} from "contracts v2/PriceConverter.sol";
+import {PriceConverter} from "fund_me_smart_contract/PriceConverter.sol";
 
 error NotOwner();
 contract FundMe {
@@ -53,4 +53,5 @@ contract FundMe {
       }
     
    
+
 }
